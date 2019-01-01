9ae9d6aa7a1da1a321ce8920b91b2a3afaa3ad00
gitlearn212 <gitlearn212@gmail.com>
2019-01-01 18:37:47 +0000

    Modified files

--- Python/Bublesort1.py
+++ Python/Bublesort1.py
@@ -1,6 +1,6 @@
 arra=[32,45,6,43,1,8,76,99,0]
-def bublesort(array):
-  for i in range(len(arra)-1,0,1):
-    for j in range(i):
-      if arra[j] > arra[j+1]:
-          arra[j],arra[j+1] > arra[j+1],arra[j]
+for i in range(len(arra)-1,0,-1):
+  for j in range(i):
+    if arra[j] > arra[j+1]:
+       arra[j],arra[j+1]=arra[j+1],arra[j]
+print(arra)
