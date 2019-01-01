7f4459423ac1ed5802222c8dc7e516bd19a8a23a
gitlearn212 <46002451+gitlearn212@users.noreply.github.com>
2019-01-01 18:26:43 +0000

    Added ( ) at print statement

--- Python/Ascending.py
+++ Python/Ascending.py
@@ -5,4 +5,4 @@ for i in range (len(lot)-1,0,-1): # for loop ie for i=lot-1,i>0,--i
  for j in range(i):
   if lot[j]> lot[j+1]:              # Swapping numbers (Ascending order)
       lot[j],lot[j+1]=lot[j+1],lot[j]
-print lot
+print (lot)
