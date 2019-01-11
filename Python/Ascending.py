5b573881e14366d4bec5c351065d259331325a6b
Suhumar <S.Alagirisamy@gre.ac.uk>
2019-01-11 14:47:32 +0000

    Adding more python files date 11-01-2019

--- Python/Ascending.py
+++ Python/Ascending.py
@@ -1,6 +1,7 @@
 # bubble sort
 
-lot=[99,1,65,2,78,349,25,199,12,0] # Array
+#lot=[99,1,65,2,78,349,25,199,12,0] # Array
+lot= [101,33,54,21,65,89,7,3,58,1,54,23,7,2]
 for i in range (len(lot)-1,0,-1): # for loop ie for i=lot-1,i>0,--i
  for j in range(i):
   if lot[j]> lot[j+1]:              # Swapping numbers (Ascending order)
---
+++ Python/Desendigorder.py
@@ -0,0 +1,14 @@
+list = [324,56,89,23,45,0,234,56,78,1,546,2,7,9,45,56,78,342,67,78,324,576,89,100]
+for x in range(len(list)-1,0,-1):
+    for y in range(x):
+        if list[y] > list[y+1]:
+            list[y],list[y+1] = list[y+1],list[y]
+print(list,)
+
+for a in range(len(list)-1,0,-1):
+    for b in range(a):
+        if list[b] > list[b+1]:
+            temp = list[b]
+            list[b+1] = lsit[b]
+            list[b] = temp
+print(list)            
