2be6e753845758da4c38623a48cbe5c94be81af3
gitlearn212 <gitlearn212@gmail.com>
2020-01-01 12:25:02 +0000

    Modifyed some codingbat files

--- CodingBat/newproject7.py
+++ CodingBat/newproject7.py
@@ -4,10 +4,12 @@ print(''' Given a non-empty string and an int n,
   The value of n will be a valid index of a char in the original string
   (i.e. n will be in the range 0..len(str)-1 inclusive). ''')
 
-def missing_char(str, n):
-  front = str[:n]   # up to but not including n
-  back = str[n+1:]  # n+1 through end of string
-  return front + back
+def missing_char(char, n):
+     
+    front = char[:n]   # up to but not including n
+    back = char[n+1:]  # n+1 through end of string
+    return front + back
+
 print(missing_char('kitten', 1))# → 'ktten'
 print(missing_char('kitten', 0))# → 'itten'
 print(missing_char('kitten', 4))# → 'kittn'
