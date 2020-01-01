4664fe6e7a07b4feadd881384a850fab43a2142b
gitlearn212 <gitlearn212@gmail.com>
2020-01-01 12:59:58 +0000

    Modiifed newproject12

--- CodingBat/newproject12.py
+++ CodingBat/newproject12.py
@@ -9,22 +9,35 @@ print(
 def pos_neg(a, b, negative):
     
     if negative:
-        #return (a < 0 and b < 0)
-        #return ((a < 0 and b > 0) or (a > 0 and b < 0))
-        # OR 
-        pass
-    #return ((a < 0 or a > 0) or (b > 0 or b < 0))
-    # OR
-    return ((a < 0 and b > 0) or (a > 0 and b < 0) or (a < 0 and b < 0))
+        return (a < 0 and b < 0)
+    return ((a < 0 and b > 0) or (a > 0 and b < 0))
+        
+
+
 '''
-# OR
-    negative = True
-    while negative:
-    #while True:
-        return ((a < 0 and b > 0) or (a > 0 and b < 0) or (a < 0 and b < 0))
-        # OR
-        #return ((a < 0 or a > 0) or (b > 0 or b < 0))
- '''   
 print(pos_neg(1, -1, False))# → True
 print(pos_neg(-1, 1, False))# → True
 print(pos_neg(-4, -5, True)) # → True
+print(pos_neg(-1, 1, True)) # False
+
+'''
+print(pos_neg(1, -1, False))# → True	True	OK	
+print(pos_neg(-1, 1, False))# → True	True	OK	
+print(pos_neg(-4, -5, True))# → True	True	OK	
+print(pos_neg(-4, -5, False))# → False	False	OK	
+print(pos_neg(-4, 5, False))# → True	True	OK	
+print(pos_neg(-4, 5, True))# → False	False	OK	
+print(pos_neg(1, 1, False))# → False	False	OK	
+print(pos_neg(-1, -1, False))# → False	False	OK	
+print(pos_neg(1, -1, True))# → False	False	OK	
+print(pos_neg(-1, 1, True))# → False	False	OK	
+print(pos_neg(1, 1, True))# → False	False	OK	
+print(pos_neg(-1, -1, True))# → True	True	OK	
+print(pos_neg(5, -5, False))# → True	True	OK	
+print(pos_neg(-6, 6, False))# → True	True	OK	
+print(pos_neg(-5, -6, False))# → False	False	OK	
+print(pos_neg(-2, -1, False))# → False	False	OK	
+print(pos_neg(1, 2, False))# → False	False	OK	
+print(pos_neg(-5, 6, True))# → False	False	OK	
+print(pos_neg(-5, -5, True))# → True	True	OK	
+
